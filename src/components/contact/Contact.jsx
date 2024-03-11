4d73ff0cb490892cdf0648022dea4fb2a7488875
AnanyaSarika <ananyasarika262@gmail.com>
2024-03-11 10:17:56 +0530

    modified contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,17 +13,17 @@ const Contact = () => {
       <div className='mr-5'>
                         <h1 className="contact-1 ">Thank You for visiting my profile!</h1>
                         <h1 className="contact-2 ">If you have any questions, inquiries, or opportunities you'd like to discuss, feel free to reach out to me.</h1>
-                        <div className='email '>
-                            <BiLogoGmail />
-                            <h1>ananyasarika262@gmail.com</h1>
+                        <div className='email'>
+                            <BiLogoGmail className='mail-logo' />
+                            <h1><a href='mailto:ananyasarika262@gmail.com' target="_blank">ananyasarika262@gmail.com</a></h1>
                         </div>
-                        <div className='Linkedin justify-center items-center flex mt-3 text-[24px]'>
-                            <BsLinkedin />
-                            <h1>ananyasarika262@gmail.com</h1>
+                        <div className=' linkedin'>
+                            <BsLinkedin className='linkedIn-logo'/>
+                            <h1><a href='https://www.linkedin.com/in/ananya-sarika-bb8647213/' target="_blank">Ananya_Sarika</a></h1>
                         </div>
-                        <div className='github justify-center items-center flex mt-3 text-[24px] '>
-                            <BsGithub />
-                            <h1>ananyasarika262@gmail.com</h1>
+                        <div className='github'>
+                            <BsGithub className='github-logo'/>
+                            <h1><a href='https://github.com/AnanyaSarika' target="_blank">AnanyaSarika</a></h1>
                         </div>
         </div>
       </div>
