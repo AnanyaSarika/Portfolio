5b4d62c136d284e988cab95a68e236ada2a85b49
AnanyaSarika <ananyasarika262@gmail.com>
2024-07-13 13:17:20 +0530

    added work exp

--- src/components/work/data.js
+++ src/components/work/data.js
@@ -1,8 +1,16 @@
 export const WORK_EXPERIENCE =[
     {
+        title: "Associate Software Engineer",
+        company:"A.P Moller-Maersk, Bengaluru", 
+        date: "July 2024 - Present",
+        responsibilities:['- GUI development using go programming language','- github actions and workflow'
+
+        ],
+    },
+    {
         title: "Database & Compute Intern",
         company:"A.P Moller-Maersk, Bengaluru", 
-        date: "June 2023 - Present",
+        date: "June 2023 - June 2024",
         responsibilities:['- Worked on data analytics, building and testing Terraform modules for infrastructure management on Azure.','- Contributed to enhancing data analytics capabilities and ensuring optimal performance through proficient database management and computational engineering skills'
 
         ],
